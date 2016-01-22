d7551a6e5ec9284601c6037f5cbd283c63ce531c
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2016-01-22 17:03:39 +0100

    parser: new ast element
    * Adds a new ast element called MalformedQuery, this will be
      used by invenio_search to interpret malformed queries into
      google-like searches.
    
    Signed-off-by: Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>

--- invenio_query_parser/ast.py
+++ invenio_query_parser/ast.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio-Query-Parser.
-# Copyright (C) 2014, 2015 CERN.
+# Copyright (C) 2014, 2015, 2016 CERN.
 #
 # Invenio-Query-Parser is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -185,4 +185,8 @@ class EmptyQuery(Leaf):
 
 
 class NotKeywordValue(Leaf):
+    pass
+
+
+class MalformedQuery(Leaf):
     pass
