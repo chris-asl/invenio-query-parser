1187ade57a0b6e9b016f79a672f4057dd11d676d
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2016-01-26 12:51:29 +0100

    utils: mappings keywords extraction
    * Keywords generator now also pulls keywords from the elasticsearch
      mapping file.
    
    * Should be merged together with inspirehep/inspire-next#783 .
    
    Signed-off-by: Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>

--- invenio_query_parser/utils.py
+++ invenio_query_parser/utils.py
@@ -23,6 +23,10 @@
 
 """invenio_query_parser tasks"""
 
+import json
+
+valid_keywords = []
+
 
 def dotter(d, key, dots):
     """ Given a json schema dictionary (d argument) returns all the properties
@@ -50,35 +54,48 @@ def dotter(d, key, dots):
 
 def get_dotted_keys(d, key, dots):
     """Removes undesirable information from extracted keywords."""
-    res = dotter(d, key, dots)
-    for i in range(len(res)):
-        res[i] = res[i][1:]
-        res[i] = str(res[i].rsplit('.', 1)[0])
-    dotted_keys = []
-    # Remove duplicates.
-    for i in res:
-        if i not in dotted_keys:
-            dotted_keys.append(i)
-    return dotted_keys
+    dotted_keys = dotter(d, key, dots)
+    return set(dotted_key[1:].rsplit('.', 1)[0] for dotted_key in dotted_keys)
 
 
-def generate_keywords():
-    import json
+def generate_valid_keywords():
+    """ Parses all files that contain valid elasticsearch keywords
+    and combines them to a list."""
+    global valid_keywords
+    in_context = True
     try:
         from invenio_base.globals import cfg
         keyword_mapping = cfg['SEARCH_ELASTIC_KEYWORD_MAPPING']
         json_schema_paths = cfg['JSON_SCHEMA_PATHS']
+        elastic_config_paths = cfg['ELASTIC_MAPPINGS_PATHS']
+        if valid_keywords:
+            return valid_keywords
     except (ImportError, RuntimeError):
+        in_context = False
         from .config import DEFAULT_KEYWORDS as keyword_mapping
-        json_schema_paths = []
+        from .config import JSON_SCHEMA_PATHS as json_schema_paths
+        from .config import ELASTIC_MAPPINGS_PATHS as elastic_config_paths
+    # Get keywords from configuration file
     keywords = keyword_mapping.keys()
     for k in keyword_mapping.values():
         if isinstance(k, dict):
             keywords += k.keys()
+    # Get keywords from the json schema
     for path in json_schema_paths:
         with open(path) as data_file:
             data = json.load(data_file)
         data = data.get('properties')
         dotted_keywords = get_dotted_keys(data, '', [])
         keywords += dotted_keywords
-    return keywords
+    # Get keywords from elasticsearch mapping
+    for path in elastic_config_paths:
+        with open(path) as data_file:
+            data = json.load(data_file)
+        data = data.get('mappings').get('record').get('properties')
+        dotted_keywords = get_dotted_keys(data, '', [])
+        keywords += dotted_keywords
+    if in_context:
+        valid_keywords = set(keywords)
+        return valid_keywords
+    else:
+        return set(keywords)
